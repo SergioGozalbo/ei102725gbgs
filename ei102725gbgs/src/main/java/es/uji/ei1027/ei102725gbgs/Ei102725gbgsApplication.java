a5e8812f4eb5f25677f7f5151e0abf334ebef94e
SergioGozalbo <al426201@uji.es>
2026-03-03 10:32:36 +0100

    Project with maven

---
+++ ei102725gbgs/src/main/java/es/uji/ei1027/ei102725gbgs/Ei102725gbgsApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.ei102725gbgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ei102725gbgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ei102725gbgsApplication.class, args);
+	}
+
+}
---
+++ ei102725gbgs/src/test/java/es/uji/ei1027/ei102725gbgs/Ei102725gbgsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.ei102725gbgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ei102725gbgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
