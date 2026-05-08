f52a7672e4c1a4b54e34389349ab31be22e9417a
Francesco Spada <francesco.spada@protonmail.com>
2026-05-08 22:17:27 +0200

    Fix wrong comment

--- src/main/java/es/uji/ei1027/ei102725gbgs/validation/package-info.java
+++ src/main/java/es/uji/ei1027/ei102725gbgs/validation/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package that contains the services of the application.
+ * Package that contains the validation classes of the application.
  */
 package es.uji.ei1027.ei102725gbgs.validation;
