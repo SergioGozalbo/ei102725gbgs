ab9b35069e7a66d0a39e755f530a7c33ac5f3de9
polina baryshnikova <al438942@uji.es>
2026-05-08 22:44:48 +0200

    Connection to the site

--- src/main/java/es/uji/ei1027/ei102725gbgs/Ei102725gbgsApplication.java
+++ src/main/java/es/uji/ei1027/ei102725gbgs/Ei102725gbgsApplication.java
@@ -1,4 +1,4 @@
-package es.uji.ei1027.ei102725gbgs.utils.request;
+package es.uji.ei1027.ei102725gbgs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
